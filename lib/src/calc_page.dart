c20a0e0ad6e3ef7a1dba989fe0b545d343ae1e02
denyssf <denys_sf@hotmail.com.br>
2023-11-18 16:16:05 -0300

    1.1 - safety rescue

---
+++ lib/src/calc_page.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: file_names
+
+// ignore: file_names
+import 'package:calc_app/src/widgets/button_hub.dart';
+import 'package:calc_app/src/widgets/display.dart';
+import 'package:flutter/material.dart';
+
+class CalcPage extends StatefulWidget {
+  const CalcPage({super.key});
+
+  @override
+  State<CalcPage> createState() => _CalcPageState();
+}
+
+class _CalcPageState extends State<CalcPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Display(value: '100'),
+          ),
+          Expanded(flex: 3, hild: ButtonHub()),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/widgets/button_hub.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class ButtonHub extends StatelessWidget {
+  const ButtonHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
